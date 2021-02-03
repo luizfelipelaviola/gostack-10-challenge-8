a20fd4727c2b85268fde32096a8ce33336ecbc87
root <root@DESKTOP-QO9V79H.localdomain>
2021-02-03 07:57:11 -0300

    (feat): challenge completed

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -16,9 +16,17 @@ interface Product {
   quantity: number;
 }
 
+interface AddProduct {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
+  quantity?: number;
+}
+
 interface CartContext {
   products: Product[];
-  addToCart(item: Omit<Product, 'quantity'>): void;
+  addToCart(item: AddProduct): void;
   increment(id: string): void;
   decrement(id: string): void;
 }
@@ -26,32 +34,84 @@ interface CartContext {
 const CartContext = createContext<CartContext | null>(null);
 
 const CartProvider: React.FC = ({ children }) => {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([
+    {
+      id: '1234',
+      title: 'Test product',
+      image_url: 'test',
+      price: 1000,
+      quantity: 0,
+    },
+  ]);
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const storage = await AsyncStorage.getItem('@items');
+      const items = storage ? JSON.parse(storage) : [];
+      if (items.length) setProducts(items);
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
-
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
-
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
-
-  const value = React.useMemo(
-    () => ({ addToCart, increment, decrement, products }),
-    [products, addToCart, increment, decrement],
+  const increment = useCallback(
+    async id => {
+      const selected_index = products.findIndex(product => product.id === id);
+      const temp_products = products;
+      if (temp_products[selected_index]) {
+        temp_products[selected_index].quantity += 1;
+      } else temp_products[selected_index].quantity = 1;
+      setProducts([...temp_products]);
+      AsyncStorage.setItem('@items', JSON.stringify([...temp_products]));
+    },
+    [setProducts, products],
   );
+
+  const addToCart = useCallback(
+    async product => {
+      const product_find = products.find(item => product.id === item.id);
+      if (product_find) {
+        increment(product_find.id);
+      } else {
+        setProducts([
+          ...products,
+          {
+            ...product,
+            quantity:
+              typeof product.quantity === 'number' ? product.quantity : 1,
+          },
+        ]);
+        AsyncStorage.setItem(
+          '@items',
+          JSON.stringify([
+            ...products,
+            {
+              ...product,
+              quantity:
+                typeof product.quantity === 'number' ? product.quantity : 1,
+            },
+          ]),
+        );
+      }
+    },
+    [setProducts, products, increment],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const selected_index = products.findIndex(product => product.id === id);
+      const temp_products = products;
+      if (temp_products[selected_index].quantity > 1)
+        temp_products[selected_index].quantity -= 1;
+      else temp_products.splice(selected_index, 1);
+
+      setProducts([...temp_products]);
+      AsyncStorage.setItem('@items', JSON.stringify([...temp_products]));
+    },
+    [setProducts, products],
+  );
+
+  const value = { addToCart, increment, decrement, products };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
